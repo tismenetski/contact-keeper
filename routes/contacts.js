1f81f859ee696f3ea649fed2874c917e4a36a1ec
tismenetski <tismenetski@gmail.com>
2020-09-05 14:30:33 +0300

    * Created Auth User & Contact models * Created routes for Auth & User , Contact add/get All * Init Client React folder

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,31 +1,78 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../middleware/auth');
+const { check, validationResult } = require('express-validator'); //Changed from 'express-validator/check' since it was deprecated
+
+//  Model
+const Contact = require('../models/Contact');
+const User = require('../models/User');
 
 //  @router GET /api/contacts
 //  @desc   Get all users contacts
 //  @access Private
-router.get('/', (req, res) => {
-  res.send('Get All users contacts');
+router.get('/', auth, async (req, res) => {
+  try {
+    const contacts = await Contact.find({ user: req.user.id }).sort({
+      date: -1,
+    });
+    res.json(contacts);
+  } catch (error) {
+    console.error(error.message);
+    res.status(500).send('Server error');
+  }
 });
 
 //  @router POST /api/contacts
 //  @desc   Add new contact
 //  @access Private
-router.post('/', (req, res) => {
-  res.send('Add contact');
-});
+router.post(
+  '/',
+  [
+    auth,
+    [
+      check('name', 'Please enter contact name').not().isEmpty(),
+      check('email', 'Please enter contact email').not().isEmpty(),
+    ],
+  ],
+
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    const { name, email, phone, type } = req.body;
+
+    try {
+      const newContact = new Contact({
+        name,
+        email,
+        phone,
+        type,
+        user: req.user.id,
+      });
+
+      const contact = await newContact.save();
+
+      res.json(contact);
+    } catch (error) {
+      console.error(error);
+      res.status(500).send('Server Error');
+    }
+  }
+);
 
 //  @router PUT /api/contacts/:id
 //  @desc   Update contact
 //  @access Private
-router.put('/:id', (req, res) => {
+router.put('/:id', auth, async (req, res) => {
   res.send('Update contact');
 });
 
 //  @router DELETE /api/contacts/:id
 //  @desc   Delete contact
 //  @access Private
-router.delete('/:id', (req, res) => {
+router.delete('/:id', auth, async (req, res) => {
   res.send('Delete contact');
 });
 
--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express');
 const app = express();
 
+//  Database Connection
+const connectDB = require('./config/db');
+connectDB();
+
+//  Init Middleware
+app.use(express.json({ extended: false })); // Accept JSON as data
+
 //  Define routes
 app.use('/api/users', require('./routes/users'));
 app.use('/api/auth', require('./routes/auth'));
 app.use('/api/contacts', require('./routes/contacts'));
 
+//  Testing Connection
 app.get('/', (req, res) => {
   res.send('Hello World');
 });
